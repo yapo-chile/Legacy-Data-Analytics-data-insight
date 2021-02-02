2338fca9a9deeabdad98269ea1212b3f7b1128e8
Catalina Sarmiento <catalina.sarmiento@adevinta.com>
2021-02-02 15:57:26 -0300

    Rollback to dm_analysis

--- re-api-daily-process/app/usecases/re_queries_op3.py
+++ re-api-daily-process/app/usecases/re_queries_op3.py
@@ -20,7 +20,7 @@ class InmoAPI3(Query):
         self.ad_params = ''
         self.emails = ''
         self.dm_table = "dm_analysis"
-        self.target_table = "real_estate_api_daily_yapo"
+        self.target_table = "dm_analysis.real_estate_api_daily_yapo"
         self.final_format = {"email": "str",
                                "date": "str",
                                "number_of_views": "Int64",
