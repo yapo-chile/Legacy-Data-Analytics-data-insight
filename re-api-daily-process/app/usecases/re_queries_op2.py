15556313b33fde625401d8dfdfccbf21adddcc33
Catalina Sarmiento <catalina.sarmiento@adevinta.com>
2021-02-04 13:28:47 -0300

    Debug batch size in dynamic option. Normalize number.

--- re-api-daily-process/app/usecases/re_queries_op2.py
+++ re-api-daily-process/app/usecases/re_queries_op2.py
@@ -90,8 +90,8 @@ class InmoAPI2(Query):
         self.logger.info("Information about emails table:")
         self.logger.info(str(self.emails))
         listid = self.emails["list_id"].tolist()
-        listid = self.chunkIt(listid, 10 + int(len(listid) % 30000))
-        self.logger.info("Batch size: {}".format(str(10 + int(len(listid) % 30000))))
+        listid = self.chunkIt(listid, 8 + int((len(listid) % 30000)/10000))
+        self.logger.info("Batch size: {}".format(str(8 + int(len(listid) % 30000))))
         for ls in listid:
             self.magnum_bullet(ls)
         del listid
