54ac7939c9d28b4983ec719c1591d47dc77e90f4
Fernando Palomera <fernando.palomera@adevinta.com>
2021-03-01 13:33:16 -0300

    added sos in segemented ads query

--- re_segmented_metrics/app/utils/query.py
+++ re_segmented_metrics/app/utils/query.py
@@ -2,52 +2,7 @@ from infraestructure.conf import getConf
 from utils.read_params import ReadParams
 
 
-class Query:
-    """
-    Class that store all querys
-    """
-    def __init__(self,
-                 conf: getConf,
-                 params: ReadParams) -> None:
-        self.params = params
-        self.conf = conf
-
-    def query_base_postgresql(self) -> str:
-        """
-        Method return str with query
-        """
-        query = """
-                select cast((now() - interval '1 day')::date as varchar)
-                    as timedate,
-	            version()  as current_version;
-            """
-        return query
-
-    def query_base_athena(self) -> str:
-        """
-        Method return str with query
-        """
-        query = """
-                select substr(
-                        cast((cast(now() as timestamp) - interval '1' day)
-                    as varchar), 1, 10) as timedate,
-                'Athena' as current_version
-            """
-        return query
-
-    def delete_base(self) -> str:
-        """
-        Method that returns events of the day
-        """
-        command = """
-                    delete from dm_analysis.db_version where 
-                    timedate::date = 
-                    '""" + self.params.get_date_from() + """'::date """
-
-        return command
-
-
-class AdViewsREQuery:
+class RESegmentedQuery:
 
     def __init__(self,
                  conf: getConf,
@@ -78,15 +33,6 @@ class AdViewsREQuery:
         group by 1,2,3
         """
         return query
-
-
-class UniqueLeadsWithOutShowPhoneREQuery:
-
-    def __init__(self,
-                 conf: getConf,
-                 params: ReadParams) -> None:
-        self.params = params
-        self.conf = conf
 
     def get_unique_leads(self) -> str:
 
@@ -120,15 +66,6 @@ class UniqueLeadsWithOutShowPhoneREQuery:
         """
         return query
 
-
-class SegmentedAdsREQuery:
-
-    def __init__(self,
-                 conf: getConf,
-                 params: ReadParams) -> None:
-        self.params = params
-        self.conf = conf
-
     def get_segmented_ads(self) -> str:
 
         query = """
@@ -150,7 +87,7 @@ class SegmentedAdsREQuery:
             platform,
             pri_pro,
             deletion_date,
-            reason_removed_detail_id_fk
+            sold_on_site
         FROM
             (
             SELECT
@@ -199,7 +136,10 @@ class SegmentedAdsREQuery:
                     ELSE 'Pro'
                 END AS pri_pro,
                 a.deletion_date::date AS deletion_date,
-                a.reason_removed_detail_id_fk
+                CASE 
+                    WHEN a.reason_removed_detail_id_fk = 1  THEN 'yes'
+                    WHEN a.deletion_date IS NOT NULL THEN 'no'
+                END AS sold_on_site
             FROM
                 ods.ad AS a
                 LEFT JOIN
