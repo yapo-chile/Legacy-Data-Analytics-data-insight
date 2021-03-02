5476d9b378b8c349c64f7af3b567bead1ec04e07
Fernando Palomera <fernando.palomera@adevinta.com>
2021-03-02 15:49:30 -0300

    edited the process script

--- re_segmented_metrics/app/usecases/process.py
+++ re_segmented_metrics/app/usecases/process.py
@@ -1,55 +1,37 @@
 # pylint: disable=no-member
 # utf-8
-from infraestructure.athena import Athena
-from infraestructure.psql import Database
-from utils.query import Query
+import logging
 from utils.read_params import ReadParams
+from usecases.active_ads import ActiveAds
+from usecases.ad_views import AdViews
+from usecases.deleted_ads import DeletedAds
+from usecases.uleads_wo_showphone import UniqueLeadsWithoutShowPhone
+from usecases.naa import NewApprovedAds
+
+    class Process:
+        def __init__(self,
+                     config,
+                     params: ReadParams,
+                     logger) -> None:
+            self.config = config
+            self.params = params
+            self.logger = logger
+
+        def generate(self):
+            self.uleads_wo_showphone = UniqueLeadsWithoutShowPhone(self.config,
+                                                                   self.params,
+                                                                   self.logger).generate()
+            self.ad_views = AdViews(self.config,
+                                          self.params,
+                                          self.logger).generate()
+            self.naa = NewApprovedAds(self.config,
+                                      self.params,
+                                      self.logger).generate()
+            self.deleted_ads = DeletedAds(self.config,
+                                          self.params,
+                                          self.logger).generate()
+            self.active_ads = ActiveAds(self.config,
+                                        self.params,
+                                        self.logger).generate()
 
 
-class Process():
-    def __init__(self,
-                 config,
-                 params: ReadParams) -> None:
-        self.config = config
-        self.params = params
-
-    # Write data to data warehouse
-    def save(self) -> None:
-        query = Query(self.config, self.params)
-        db = Database(conf=self.config.db)
-        db.execute_command(query.delete_base())
-        db.insert_data(self.data_athena)
-        db.insert_data(self.data_dwh)
-        db.close_connection()
-
-    # Query data from data warehouse
-    @property
-    def data_dwh(self):
-        return self.__data_dwh
-
-    @data_dwh.setter
-    def data_dwh(self, config):
-        query = Query(config, self.params)
-        db_source = Database(conf=config)
-        data_dwh = db_source.select_to_dict(query\
-                                            .query_base_postgresql())
-        db_source.close_connection()
-        self.__data_dwh = data_dwh
-
-    # Query data from Pulse bucket
-    @property
-    def data_athena(self):
-        return self.__data_athena
-
-    @data_athena.setter
-    def data_athena(self, config):
-        athena = Athena(conf=config)
-        query = Query(config, self.params)
-        data_athena = athena.get_data(query.query_base_athena())
-        athena.close_connection()
-        self.__data_athena = data_athena
-
-    def generate(self):
-        self.data_dwh = self.config.db
-        self.data_athena = self.config.athenaConf
-        self.save()
