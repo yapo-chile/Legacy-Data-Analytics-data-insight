9a3d46ff38320151586cac28e2d5ae0bb477a00d
Fernando Palomera <fernando.palomera@adevinta.com>
2021-03-03 16:38:45 -0300

    fix logger param

--- re_segmented_metrics/app/main.py
+++ re_segmented_metrics/app/main.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
     logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
     PARAMS = ReadParams(sys.argv)
     TIME.get_time()
+
     # Calling main process
     Process(CONFIG, PARAMS, LOGGER).generate()
     # End process
