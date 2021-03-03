fe8703f4b6ba35338e723dcd7f962f4f5fa86cb5
Fernando Palomera <fernando.palomera@adevinta.com>
2021-03-03 14:10:22 -0300

    fix identitation problem

--- re_segmented_metrics/app/usecases/process.py
+++ re_segmented_metrics/app/usecases/process.py
@@ -8,30 +8,31 @@ from usecases.deleted_ads import DeletedAds
 from usecases.uleads_wo_showphone import UniqueLeadsWithoutShowPhone
 from usecases.naa import NewApprovedAds
 
-    class Process:
-        def __init__(self,
-                     config,
-                     params: ReadParams,
-                     logger) -> None:
-            self.config = config
-            self.params = params
-            self.logger = logger
 
-        def generate(self):
-            self.uleads_wo_showphone = UniqueLeadsWithoutShowPhone(self.config,
-                                                                   self.params,
-                                                                   self.logger).generate()
-            self.ad_views = AdViews(self.config,
-                                          self.params,
-                                          self.logger).generate()
-            self.naa = NewApprovedAds(self.config,
+class Process:
+    def __init__(self,
+                 config,
+                 params: ReadParams,
+                 logger) -> None:
+        self.config = config
+        self.params = params
+        self.logger = logger
+
+    def generate(self):
+        self.uleads_wo_showphone = UniqueLeadsWithoutShowPhone(self.config,
+                                                               self.params,
+                                                               self.logger).generate()
+        self.ad_views = AdViews(self.config,
                                       self.params,
                                       self.logger).generate()
-            self.deleted_ads = DeletedAds(self.config,
-                                          self.params,
-                                          self.logger).generate()
-            self.active_ads = ActiveAds(self.config,
-                                        self.params,
-                                        self.logger).generate()
+        self.naa = NewApprovedAds(self.config,
+                                  self.params,
+                                  self.logger).generate()
+        self.deleted_ads = DeletedAds(self.config,
+                                      self.params,
+                                      self.logger).generate()
+        self.active_ads = ActiveAds(self.config,
+                                    self.params,
+                                    self.logger).generate()
 
 
